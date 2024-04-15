daa288e785bb8ee9450787bff1bd102a01e49687
Andrew S. <asenger@gmail.com>
2024-04-14 22:57:54 -0400

    update on_load to show git info

--- main.py
+++ main.py
@@ -34,20 +34,48 @@ async def reload(ctx, extension):
 
 @bot.event
 async def on_ready():
+    # Show list of connected servers to console.
     print(f"Logged in as {bot.user} in the following servers:")
     for guild in bot.guilds:
         print(f"   {guild.name} (id: {guild.id})")
     print()
+    
+    # Show list of loaded modules to the console.
     print(f"Loading the following modules:")
     for filename in os.listdir('fg_discord'):
         if filename.endswith('.py'):
             print(f"   fg_discord.{filename[:-3]}")
             await bot.load_extension(f'fg_discord.{filename[:-3]}')
-    git_dis1 = subprocess.getoutput("git dis1").replace(" | ","\n")
-    git_dis2 = subprocess.getoutput("git dis2")
-    await send_log(None, f"# BOT STARTED\n{git_dis1}\n\n**Diff List:**\n```{git_dis2 if git_dis2 else 'No updates.'}```")
-    # Adding this to test changes to remote.
+    
+    # Get git info
+    git_branch_local = subprocess.getoutput("git branch --show-current")
+    git_id_local = subprocess.getoutput(f"git rev-parse --short {git_branch_local}")
+    git_id_remote = subprocess.getoutput(f"git rev-parse --short origin/{git_branch_local}")
+    git_commit_desc = subprocess.getoutput(f"git log -1 --pretty=%B")
+    git_diff_list = subprocess.getoutput("git dis2")
 
+    if git_id_local != git_id_remote:
+        git_id_msg = "*** DIFFERENT ***"
+    else:
+        git_id_msg = ""
+
+    # Generate git info text
+    git_txt = f"# BOT STARTED\n"
+    git_txt += f"```"
+    git_txt += f"Branch:     {git_branch_local}\n"
+    git_txt += f"\n"
+    git_txt += f"Commit IDs:         {git_id_msg}\n     local: {git_id_local}\n    remote: {git_id_remote}\n"
+    git_txt += f"\n"
+    git_txt += f"Commit Description:\n"
+    git_txt += f"   {git_commit_desc}\n"
+    git_txt += f"Diff List:\n"
+    git_txt += f"{git_diff_list}"
+    git_txt += f"```"
+
+    # Send git info text to log channel
+    await send_log(None, f"{git_txt}")
+
+    # Start loop for checking tournaments
     check_tournaments.start()
 
 @tasks.loop(seconds=15)
