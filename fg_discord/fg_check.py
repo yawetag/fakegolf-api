c2eb8b3134361cb6fbbf5eb3de71169b79296f2a
Andrew S. <asenger@gmail.com>
2024-04-28 16:26:14 -0400

    status check 304

--- fg_discord/fg_check.py
+++ fg_discord/fg_check.py
@@ -33,6 +33,7 @@ async def ck_check_tournaments(bot, ts):
     await ck_tourn_258(bot, ts, c, t_list)
     await ck_tourn_300(bot, ts, c, t_list)
     await ck_tourn_302(bot, ts, c, t_list)
+    await ck_tourn_304(bot, ts, c, t_list)
     
     return
 
@@ -224,6 +225,29 @@ async def ck_tourn_302(bot, ts, c, t_list):
                 if guild.get_member(int(r['discord_snowflake'])) is None:    # If user is not active in server, remove them.
                     rem_user = db.remove_user_from_tournament(t['id'], r['id'])
                     log_ms = f"<t:{ts}:T> **{t['tournament_name']}** [{t['id']}] Inactive User: **{r['id']} - <@{r['discord_snowflake']}>** | Removed from tournament."
+                    await send_log(None, log_ms)
+            
+            db.change_tournament_status(t['id'], ci['next_status'])         # Change to next code
+            await log_msg(ts, t, ci, c)
+
+async def ck_tourn_304(bot, ts, c, t_list):
+    """
+    Tournament Status 304 Check.
+    For each user in a tournament, check they are not banned.
+    If they are, remove them from the tournament.
+    Afterward, move status to next status.
+    """
+    ci = c[304]
+
+    await print_log(ci)
+
+    for t in t_list:
+        if t['status_id'] == ci['id']:
+            reg_users = db.get_tournament_user_info(t['id'])
+            for r in reg_users:
+                if r['is_banned'] == 1:     # If user is banned from playing, remove them.
+                    rem_user = db.remove_user_from_tournament(t['id'], r['id'])
+                    log_ms = f"<t:{ts}:T> **{t['tournament_name']}** [{t['id']}] Banned User: **{r['id']} - <@{r['discord_snowflake']}>** | Removed from tournament."
                     await send_log(None, log_ms)
             
             db.change_tournament_status(t['id'], ci['next_status'])         # Change to next code
